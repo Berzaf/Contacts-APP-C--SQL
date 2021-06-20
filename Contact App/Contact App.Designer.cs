2ea68beb205f7619b3fbaac516b77308353c4bae
Berzaf <berzafastroo2012@gmail.com>
2021-06-19 22:11:11 -0500

    web form

---
+++ Contact App/Contact App.Designer.cs
@@ -0,0 +1,308 @@
+﻿
+namespace Contact_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblContact = new System.Windows.Forms.Label();
+            this.lblFname = new System.Windows.Forms.Label();
+            this.textBoxFname = new System.Windows.Forms.TextBox();
+            this.textBoxLname = new System.Windows.Forms.TextBox();
+            this.lblLname = new System.Windows.Forms.Label();
+            this.textBoxPnum = new System.Windows.Forms.TextBox();
+            this.lblPnum = new System.Windows.Forms.Label();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblContact
+            // 
+            this.lblContact.AutoSize = true;
+            this.lblContact.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblContact.Font = new System.Drawing.Font("Georgia", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblContact.ForeColor = System.Drawing.Color.Black;
+            this.lblContact.Location = new System.Drawing.Point(362, 60);
+            this.lblContact.Name = "lblContact";
+            this.lblContact.Size = new System.Drawing.Size(220, 35);
+            this.lblContact.TabIndex = 0;
+            this.lblContact.Text = "Contacts App";
+            this.lblContact.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblFname
+            // 
+            this.lblFname.AutoSize = true;
+            this.lblFname.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblFname.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.lblFname.Location = new System.Drawing.Point(113, 176);
+            this.lblFname.Name = "lblFname";
+            this.lblFname.Size = new System.Drawing.Size(116, 24);
+            this.lblFname.TabIndex = 1;
+            this.lblFname.Text = "First Name:";
+            // 
+            // textBoxFname
+            // 
+            this.textBoxFname.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxFname.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBoxFname.Location = new System.Drawing.Point(334, 173);
+            this.textBoxFname.Name = "textBoxFname";
+            this.textBoxFname.Size = new System.Drawing.Size(248, 30);
+            this.textBoxFname.TabIndex = 2;
+            // 
+            // textBoxLname
+            // 
+            this.textBoxLname.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxLname.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBoxLname.Location = new System.Drawing.Point(334, 270);
+            this.textBoxLname.Name = "textBoxLname";
+            this.textBoxLname.Size = new System.Drawing.Size(248, 30);
+            this.textBoxLname.TabIndex = 4;
+            // 
+            // lblLname
+            // 
+            this.lblLname.AutoSize = true;
+            this.lblLname.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblLname.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.lblLname.Location = new System.Drawing.Point(113, 273);
+            this.lblLname.Name = "lblLname";
+            this.lblLname.Size = new System.Drawing.Size(112, 24);
+            this.lblLname.TabIndex = 3;
+            this.lblLname.Text = "Last Name:";
+            // 
+            // textBoxPnum
+            // 
+            this.textBoxPnum.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxPnum.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBoxPnum.Location = new System.Drawing.Point(334, 360);
+            this.textBoxPnum.Name = "textBoxPnum";
+            this.textBoxPnum.Size = new System.Drawing.Size(248, 30);
+            this.textBoxPnum.TabIndex = 6;
+            // 
+            // lblPnum
+            // 
+            this.lblPnum.AutoSize = true;
+            this.lblPnum.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPnum.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.lblPnum.Location = new System.Drawing.Point(113, 363);
+            this.lblPnum.Name = "lblPnum";
+            this.lblPnum.Size = new System.Drawing.Size(152, 24);
+            this.lblPnum.TabIndex = 5;
+            this.lblPnum.Text = "Phone Number:";
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxAddress.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBoxAddress.Location = new System.Drawing.Point(334, 460);
+            this.textBoxAddress.Multiline = true;
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(248, 114);
+            this.textBoxAddress.TabIndex = 8;
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblAddress.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.lblAddress.Location = new System.Drawing.Point(104, 489);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(87, 24);
+            this.lblAddress.TabIndex = 7;
+            this.lblAddress.Text = "Address:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnAdd.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.btnAdd.Location = new System.Drawing.Point(89, 633);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(94, 51);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Aqua;
+            this.btnReset.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnReset.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.btnReset.Location = new System.Drawing.Point(520, 633);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(94, 51);
+            this.btnReset.TabIndex = 10;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.Yellow;
+            this.btnUpdate.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnUpdate.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.btnUpdate.Location = new System.Drawing.Point(235, 633);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(94, 51);
+            this.btnUpdate.TabIndex = 11;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Red;
+            this.btnDelete.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnDelete.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.btnDelete.Location = new System.Drawing.Point(376, 633);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(94, 51);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnSave.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSave.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.btnSave.Location = new System.Drawing.Point(320, 727);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(121, 53);
+            this.btnSave.TabIndex = 13;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(627, 249);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(535, 325);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.textBoxSearch.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxSearch.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBoxSearch.Location = new System.Drawing.Point(627, 179);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(432, 30);
+            this.textBoxSearch.TabIndex = 15;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.BackColor = System.Drawing.Color.Gray;
+            this.lblSearch.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblSearch.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.lblSearch.Location = new System.Drawing.Point(1092, 182);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(70, 24);
+            this.lblSearch.TabIndex = 16;
+            this.lblSearch.Text = "Search";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Red;
+            this.btnExit.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnExit.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.btnExit.Location = new System.Drawing.Point(1101, 26);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(94, 51);
+            this.btnExit.TabIndex = 17;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(1246, 808);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.textBoxPnum);
+            this.Controls.Add(this.lblPnum);
+            this.Controls.Add(this.textBoxLname);
+            this.Controls.Add(this.lblLname);
+            this.Controls.Add(this.textBoxFname);
+            this.Controls.Add(this.lblFname);
+            this.Controls.Add(this.lblContact);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Contact App";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblContact;
+        private System.Windows.Forms.Label lblFname;
+        private System.Windows.Forms.TextBox textBoxFname;
+        private System.Windows.Forms.TextBox textBoxLname;
+        private System.Windows.Forms.Label lblLname;
+        private System.Windows.Forms.TextBox textBoxPnum;
+        private System.Windows.Forms.Label lblPnum;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
